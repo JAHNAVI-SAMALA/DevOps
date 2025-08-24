7a6a4b659734ff35a3d0b83186a945181af65819
SAMALA JAHNAVI <jahnavisamala256@gmail.com>
2025-08-24 11:31:50 +0530

    Update SampleName.java

--- SampleName.java
+++ SampleName.java
@@ -1,6 +1,8 @@
 public class SampleName {
     public static void main(String[] args) {
-        String name = "JahnaviSamala";
+        String name = "Jahnavi Samala";
+        String rollno = "23D21A05H4";
         System.out.println("Hello, my name is " + name);
+        System.out.println("My roll number is " + rollno);
     }
 }
